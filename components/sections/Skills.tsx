7c52f42a395197b1cca9c526ceb7d298fdbdf5a4
renish47 <ineroff77@gmail.com>
2023-09-27 17:33:09 +0530

    fix bugs

--- components/sections/Skills.tsx
+++ components/sections/Skills.tsx
@@ -40,7 +40,7 @@ export default function Skills() {
       className="mb-28 max-w-[53rem]  scroll-mt-28 text-center sm:mb-40"
     >
       <SectionHeading>My skills</SectionHeading>
-      <ul className="flex flex-wrap justify-center gap-3 text-lg text-gray-800">
+      <ul className="flex flex-wrap justify-center gap-3 text-lg text-gray-800 sm:gap-2">
         {skillsData.map((skill, index) => (
           <motion.li
             className="cursor-default rounded-md border border-black/[0.15] bg-white px-5 py-3 shadow-md transition dark:bg-white/10 dark:text-white/80"
