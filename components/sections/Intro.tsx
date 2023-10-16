8cfe78db2c88e305c304f753ecd597e89c6f3b38
renish47 <ineroff77@gmail.com>
2023-10-16 20:31:33 +0530

    update resume

--- components/sections/Intro.tsx
+++ components/sections/Intro.tsx
@@ -83,7 +83,7 @@ export default function Intro() {
 
         <a
           className="group flex cursor-pointer items-center gap-2 rounded-full border border-black/20 bg-white px-7 py-3 shadow-md shadow-black/[0.05] outline-none transition hover:scale-110 active:scale-105 dark:bg-white/10"
-          href="https://drive.google.com/file/d/1t5KvNH3InjJeCqMyu5gdfEFYhCYDG23v/view?usp=sharing"
+          href="https://drive.google.com/file/d/10id7i3sE8Auz0PKvxYZqu17B2GlQNHhk/view?usp=sharing"
           target="_blank"
         >
           Download Resume{" "}
